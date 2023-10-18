c0b70ef65bf1aca5a4610c4af1b2f71f56cf1943
Marcos Paulo <marcospaulosousa48@gmail.com>
2023-10-18 09:14:17 -0300

    feat: add trip highlights component

---
+++ src/app/trips/[tripId]/components/TripHighlights.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+
+interface TripHighlightsProps {
+  highlights: string[];
+}
+
+const TripHighlights = ({ highlights }: TripHighlightsProps) => {
+  return (
+    <div className="flex flex-col p-5">
+      <h2 className="font-semibold text-primaryDarker mb-2 ">Destaques</h2>
+
+      <div className="flex flex-wrap gap-y-3">
+        {highlights.map((highlight, index) => (
+          <div className="flex items-center gap-2 w-1/2">
+            <AiOutlineCheckCircle size={20} className="text-primary" />
+            <p className="text-grayPrimary text-xs">{highlight}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TripHighlights;
--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import TripHeader from "./components/TripHeader";
 import TripReservation from "./components/TripReservation";
 import TripDescription from "./components/TripDescription";
+import TripHighlights from "./components/TripHighlights";
 
 const getTripDetails = async (tripId: string) => {
   let trip;
@@ -28,6 +29,7 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
       <TripHeader trip={trip} />
       <TripReservation trip={trip} />
       <TripDescription description={trip.description} />
+      <TripHighlights highlights={trip.highlights} />
     </div>
   );
 };
