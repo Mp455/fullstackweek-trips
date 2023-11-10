4f3ea95c7ecfb2e74ced1ad7e0c406df5269b37e
Marcos Paulo <marcospaulosousa48@gmail.com>
2023-11-10 09:09:40 -0300

    feat: adjusting responsiveness for other screens

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+
 import React from "react";
 import TripHeader from "./components/TripHeader";
 import TripReservation from "./components/TripReservation";
@@ -7,16 +8,11 @@ import TripHighlights from "./components/TripHighlights";
 import TripLocation from "./components/TripLocation";
 
 const getTripDetails = async (tripId: string) => {
-  let trip;
-  try {
-    trip = await prisma.trip.findUnique({
-      where: {
-        id: tripId,
-      },
-    });
-  } finally {
-    await prisma.$disconnect();
-  }
+  const trip = await prisma.trip.findUnique({
+    where: {
+      id: tripId,
+    },
+  });
 
   return trip;
 };
@@ -25,20 +21,28 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
   const trip = await getTripDetails(params.tripId);
 
   if (!trip) return null;
+
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto lg:px-40 lg:pt-10">
       <TripHeader trip={trip} />
-      <TripReservation
-        tripId={trip.id}
-        pricePerDay={trip.pricePerDay as any}
-        maxGuests={trip.maxGuests}
-        tripStartDate={trip.startDate}
-        tripEndDate={trip.endDate}
-      />
-      <TripDescription description={trip.description} />
-      <TripHighlights highlights={trip.highlights} />
+      <div className="flex flex-col lg:flex-row lg:mt-12 lg:gap-20">
+        <div className="lg:order-2">
+          <TripReservation
+            tripId={trip.id}
+            pricePerDay={trip.pricePerDay as any}
+            tripStartDate={trip.startDate}
+            tripEndDate={trip.endDate}
+            maxGuests={trip.maxGuests}
+          />
+        </div>
+
+        <div className="lg:order-1">
+          <TripDescription description={trip.description} />
+          <TripHighlights highlights={trip.highlights} />
+        </div>
+      </div>
       <TripLocation
-        locationDescription={trip.locationDescription}
+        locationDescription={trip.locationDescription!!}
         location={trip.location}
       />
     </div>
